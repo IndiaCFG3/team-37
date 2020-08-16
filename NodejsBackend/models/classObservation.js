92dc9f5dd8ef60d45b7fe16887d379d7579f9fb6
ashwini99 <ashwini9999>
2020-08-16 14:43:25 +0530

    creating classObservation model

---
+++ NodejsBackend/models/classObservation.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const classObservation = sequelize.define('',{
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    student:{
+        name:{
+            type:Sequelize.STRING,
+            allowNull: false
+        },
+        roll_no:{
+            type:Sequelize.STRING,
+            allowNull: false
+        },
+        team_no:{
+            type:Sequelize.STRING,
+            allowNull:false
+            
+        },
+        leadership:{
+            type:Sequelize.STRING,
+            allowNull:false
+        },
+        mentorship:{
+            type:Sequelize.STRING,
+            allowNull: false
+        }
+    }
+});
+
+module.exports = classObservation;
